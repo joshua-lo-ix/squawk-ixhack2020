503dc6517b942946bddd123c81d30faa1c2f5dbc
Joshua Lo <joshua.lo@indexexchange.com>
2020-11-23 14:15:10 -0500

    Migrate ansible stuff out of this repo (less pull hell)

--- squawk_server.go
+++ squawk_server.go
@@ -12,11 +12,18 @@ func main() {
 }
 
 func refresh_ansible() {
-    cmd := exec.Command("git", "fetch", "", "https://github.com/joshua-lo-ix/squawk-ixhack2020.git")
+    cmd := exec.Command("rm", "-rf", "squawk-ixhack2020-ansible")
+    if err := cmd.Run(); err != nil {
+        log.Fatal(err)
+    }
+    cmd := exec.Command("git", "clone", "https://github.com/joshua-lo-ix/squawk-ixhack2020-ansible.git")
+    if err := cmd.Run(); err != nil {
+        log.Fatal(err)
+    }
 }
 
 func exec_ansible() {
-    cmd := exec.Command("ansible-playbook", "-i", "squawk-ixhack2020/ansible/hosts", "squawk-ixhack2020/ansible/squawk-playbook.yml")
+    cmd := exec.Command("ansible-playbook", "-i", "squawk-ixhack2020-ansible/hosts", "squawk-ixhack2020-ansible/squawk-playbook.yml")
     if err := cmd.Run(); err != nil {
         log.Fatal(err)
     }
