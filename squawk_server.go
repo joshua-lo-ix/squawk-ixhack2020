2bdcd09ac639dc128d435a5c659ea046b97d836b
Joshua Lo <joshua.lo@indexexchange.com>
2020-11-23 14:04:49 -0500

    Serve on 8081

--- squawk_server.go
+++ squawk_server.go
@@ -7,12 +7,17 @@ import (
 )
 
 func main() {
+    http.HandleFunc("/", handler)
+    log.Fatal(http.ListenAndServe(":8081", nil))
+}
+
+func refresh_ansible() {
+    cmd := exec.Command("git", "fetch", "", "https://github.com/joshua-lo-ix/squawk-ixhack2020.git")
+}
+
+func exec_ansible() {
     cmd := exec.Command("ansible-playbook", "-i", "squawk-ixhack2020/ansible/hosts", "squawk-ixhack2020/ansible/squawk-playbook.yml")
     if err := cmd.Run(); err != nil {
         log.Fatal(err)
     }
-}
-
-func refresh_ansible() {
-    cmd := exec.Command("git", "clone", "https://github.com/joshua-lo-ix/squawk-ixhack2020.git")
 }
