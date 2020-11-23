2fb31499168e88b6e4f396d5addcc7e992755e33
Joshua Gross <joshua.gross@indexexchange.com>
2020-11-23 14:54:08 -0500

    adding the modal example

--- squawk_server.go
+++ squawk_server.go
@@ -4,24 +4,32 @@ import (
 	"flag"
 	"fmt"
 	"log"
-    "net/http"
-    "os/exec"
+	"net/http"
+	"os/exec"
 )
 
 var version string
+var signingsecret string
+var slacktoken string
 
 func main() {
+
 	flag.StringVar(&version, "version", "0", "the git commit of this build")
+	flag.StringVar(&signingsecret, "signingsecret", "0", "Signing secret for slack")
+	flag.StringVar(&slacktoken, "slacktoken", "0", "slack tokent for slack")
+
 	flag.Parse()
 	http.HandleFunc("/", handler)
-    http.HandleFunc("/version", versionHandler)
-    http.HandleFunc("/ansibletest", ansibleTest)
+	http.HandleFunc("/version", versionHandler)
+	http.HandleFunc("/ansibletest", ansibleTest)
+	http.HandleFunc("/slash", handleSlash)
+	http.HandleFunc("/modal", handleModal)
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
 func ansibleTest(w http.ResponseWriter, r *http.Request) {
-    refresh_ansible()
-    exec_ansible()
+	refresh_ansible()
+	exec_ansible()
 }
 func versionHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, version)
@@ -31,27 +39,27 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func refresh_ansible() {
-    clean_ansible()
-    get_ansible()
+	clean_ansible()
+	get_ansible()
 }
 
 func clean_ansible() {
-    cmd := exec.Command("rm", "-rf", "squawk-ixhack2020-ansible")
-    if err := cmd.Run(); err != nil {
-        log.Fatal(err)
-    }
+	cmd := exec.Command("rm", "-rf", "squawk-ixhack2020-ansible")
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func get_ansible() {
-    cmd := exec.Command("git", "clone", "https://github.com/joshua-lo-ix/squawk-ixhack2020-ansible.git")
-    if err := cmd.Run(); err != nil {
-        log.Fatal(err)
-    }
+	cmd := exec.Command("git", "clone", "https://github.com/joshua-lo-ix/squawk-ixhack2020-ansible.git")
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func exec_ansible() {
-    cmd := exec.Command("ansible-playbook", "-i", "squawk-ixhack2020-ansible/hosts", "squawk-ixhack2020-ansible/squawk-playbook.yml")
-    if err := cmd.Run(); err != nil {
-        log.Fatal(err)
-    }
+	cmd := exec.Command("ansible-playbook", "-i", "squawk-ixhack2020-ansible/hosts", "squawk-ixhack2020-ansible/squawk-playbook.yml")
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
 }
